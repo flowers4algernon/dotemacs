49656b376b8e7ec5dc88fb471534e9c376bf1d1a
Geoff Wark <gtw@airmail.cc>
2017-06-28 13:19:23 -0400

    Initialize repository

---
+++ init.el
@@ -0,0 +1,5 @@
+;; Keep all backups/auto-saves in $TMPDIR
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
