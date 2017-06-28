edc05312ba92f45805d046ef726b7de581a7de3a
Geoff Wark <gtw@airmail.cc>
2017-06-28 16:13:16 -0400

    Add header/footer

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
+;;; init.el --- The first thing GNU Emacs runs
+
 ;; Keep all backups/auto-saves in $TMPDIR
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;;; init.el ends here
