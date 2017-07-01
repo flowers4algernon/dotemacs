2918a67df5c1cd238c0de46a025fb853a7e2551d
Geoff Wark <gtw@airmail.cc>
2017-07-01 12:50:01 -0400

    Change garbage collection behaviour

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 ;;; init.el --- The first thing GNU Emacs runs
 
+;; Do NOT run garbage collection during startup by setting the value super high.
+;; This drastically improves `emacs-init-time'.
+;; NOTE: We revert this at the end of the file.
+(setq gc-cons-threshold 999999999999)
+
 ;; Keep all backups/auto-saves in $TMPDIR
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
@@ -9,5 +14,11 @@
 ;; Save customizations to their own file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file :noerror)
+
+;; Revert garbage collection behaviour to a more modern level
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq gc-cons-threshold 20000000))) ; magnars
 
 ;;; init.el ends here
